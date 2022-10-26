776cc640e2bbfe211ea967e21d097f9f6792f620
muttas <93790510+muttas@users.noreply.github.com>
2022-10-26 17:19:59 +0530

    Create Assignment1_1.java

---
+++ Assignment1_1.java
@@ -0,0 +1,141 @@
+public class Assignment1_1 {
+
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+
+            int n=9;
+
+            for(int i=0;i<n; i++)
+            {
+                for(int j=0 ; j<n; j++)
+                {
+                    if(i==0 && j<(n-1) || i==n-1 && j<(n-1) || j==(n-1)/2)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+
+                }
+                for(int j=0 ; j<n/2; j++)
+                {
+                    System.out.print(" ");
+
+                }
+                for(int j=0 ; j<n; j++)
+                {
+                    if(j==0 || j==n-1 || i==j
+                    )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+
+
+                }
+                for(int j=0 ; j<n/2; j++)
+                {
+                    System.out.print(" ");
+
+                }
+                for(int j=0 ; j<n; j++)
+                {
+                    if(j==0||i==0||i==(n-1)/2||i==n-1)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+
+                }
+                for(int j=0 ; j<n/2; j++)
+                {
+                    System.out.print(" ");
+
+                }
+                for(int j=0 ; j<n; j++)
+                {
+                    if(j==0||j==n-1||i==n-1)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+
+                }
+                for(int j=0 ; j<n/2; j++)
+                {
+                    System.out.print(" ");
+
+                }
+                for(int j=0 ; j<n; j++)
+                {
+                    if(j==0||i==0||j==(n-1) && i<(n-1)/2 || i==j && i>(n-1)/2|| i==(n-1)/2 && j<(n-1)/2)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+
+                }
+                for(int j=0 ; j<n/2; j++)
+                {
+                    System.out.print(" ");
+
+                }
+                for(int j=0 ; j<n; j++)
+                {
+                    if(j==0||j==n-1||i==n-1||i==0)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+
+                }
+                for(int j=0 ; j<n/2; j++)
+                {
+                    System.out.print(" ");
+
+                }
+                for(int j=0 ; j<n; j++)
+                {
+                    if(j==0 || j==n-1 || i==j
+                    )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+
+                    }
+                }
+                System.out.println();
+            }
+
+
+        }
+
+    }
+
